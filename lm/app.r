ff53b34e5bbf33de60a9a92f8957fff2e71fa908
hollingerj <hol283@osumc.edu>
2024-07-19 12:40:26 -0400

    Update app.r

--- lm/app.r
+++ lm/app.r
@@ -1,63 +1,54 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    
+    titlePanel("Shiny LM Data"),
 
-    # Sidebar with a slider input for number of bins 
+ 
     sidebarLayout(
         sidebarPanel(
 
             
-            # Input: Select a file ----
             fileInput("file1", "Choose CSV File",
                       multiple = FALSE,
                       accept = c("text/csv",
                                  "text/comma-separated-values,text/plain",
                                  ".csv")),
             
-            # Horizontal line ----
+           
             tags$hr(),
             
-            # Input: Checkbox if file has header ----
+            
             checkboxInput("header", "Header", TRUE),
             
-            # Input: Select separator ----
+           
             radioButtons("sep", "Separator",
                          choices = c(Comma = ",",
                                      Semicolon = ";",
                                      Tab = "\t"),
                          selected = ","),
             
-            # Input: Select quotes ----
+      
             radioButtons("quote", "Quote",
                          choices = c(None = "",
                                      "Double Quote" = '"',
                                      "Single Quote" = "'"),
                          selected = '"'),
             
-            # Horizontal line ----
+           
             tags$hr(),
             
-            # Input: Select number of rows to display ----
+            
             radioButtons("disp", "Display",
                          choices = c(Head = "head",
                                      All = "all"),
-                         selected = "head")
+                         selected = "head"),
+            actionButton("go", "Plot Linear Model")
         ),
 
-        # Show a plot of the generated distribution
+       
         mainPanel(
            plotOutput("distPlot"),
            plotOutput("lmPlot"),
@@ -66,8 +57,10 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
+
 server <- function(input, output) {
+
+    lmdata <- reactiveValues()
 
     dataInput <- reactive({
         req(input$file1)
@@ -78,42 +71,60 @@ server <- function(input, output) {
                        quote = input$quote)
         return(df)
     })
-    
-    # output$distPlot <- renderPlot({
-    #     # generate bins based on input$bins from ui.R
-    #     x    <- faithful[, 2]
-    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    #     print(bins)
-    #     # draw the histogram with the specified number of bins
-    #     hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    # })
-    # 
-    
+
+    observeEvent(input$go,{
+        update_lm()
+    })
+
+    update_lm <- function(){
+        lmdata$model <- lm(y ~ x, data = dataInput())
+        lmdata$rsq <- summary(lmdata$model)$r.squared
+        lmdata$coef <- summary(lmdata$model)$coefficients
+        
+        # Debugging print statements
+        print("Linear model updated:")
+        print(lmdata$model)
+        print("Coefficients:")
+        print(lmdata$coef)
+    }
+
     output$distPlot <- renderPlot({
-        plot(dataInput()$x,dataInput()$y)
+        print("Rendering scatter plot...")
+        print(dataInput())
+        plot(dataInput()$x, dataInput()$y, xlab = "X Axis", ylab = "Y Axis")
     })
-    
-    output$lmtPlot <- renderPlot({
-        plot(dataInput()$x,dataInput()$y)
+
+    output$lmPlot <- renderPlot({
+        req(dataInput())
+        print("Rendering lm plot...")
+        print(lmdata$model)
+        
+        # Plot scatter plot
+        plot(dataInput()$x, dataInput()$y, xlab = "X Axis", ylab = "Y Axis")
+        
+        # Check if lmdata$model exists
+        if (!is.null(lmdata$model)) {
+            # Add linear model line
+            abline(lmdata$model, col = "blue")
+            
+            # Extract coefficients
+            slope <- round(lmdata$coef[2], 2)
+            intercept <- round(lmdata$coef[1], 2)
+            correlation <- round(cor(dataInput()$x, dataInput()$y), 2)
+            
+            # Display coefficients as text on the plot
+            legend("topright", legend = c(paste("Slope:", slope), paste("Intercept:", intercept), paste("Correlation:", correlation)), 
+                   bty = "n", cex = 0.8, text.col = "black")
+        }
     })
-    
-    
+
     output$contents <- renderTable({
-        
-        # input$file1 will be NULL initially. After the user selects
-        # and uploads a file, head of that data file by default,
-        # or all rows if selected, will be shown.
-        
-        
         if(input$disp == "head") {
             return(head(dataInput()))
-        }
-        else {
+        } else {
             return(dataInput())
         }
-        
     })
-        
 }
 
 # Run the application 
